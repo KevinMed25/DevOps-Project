2a6b43e0f46f13068561e4d9d5ed038fc93b7718
BeckyZhuWu <zhuwub@gmail.com>
2025-04-30 00:38:37 -0600

    feat: create drivers endpoint

--- app/controllers/drivers.py
+++ app/controllers/drivers.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from app.services import DriversService
 from http import HTTPStatus
+from app.models.drivers import DriverSchema
+
 
 drivers_blueprint = Blueprint('drivers', __name__)
 
@@ -9,3 +11,11 @@ def get_drivers():
     driversService = DriversService()
     resp = driversService.getAllDrivers()
     return jsonify(resp), HTTPStatus.OK
+
+@drivers_blueprint.route('/', methods=['POST'])
+def create_driver():
+    driver = request.get_json()
+    driversService = DriversService()
+    resp = driversService.createDriver(DriverSchema(**driver))
+    return jsonify(resp), HTTPStatus.OK
+
