fc4b256c8e42f16d6b2c0ed79d4a29aa05489fb2
BeckyZhuWu <zhuwub@gmail.com>
2025-04-30 13:38:14 -0600

    feat:update driver

--- app/controllers/drivers.py
+++ app/controllers/drivers.py
@@ -19,3 +19,13 @@ def create_driver():
     resp = driversService.createDriver(DriverSchema(**driver))
     return jsonify(resp), HTTPStatus.OK
 
+@drivers_blueprint.route('/<int:id>', methods=['PUT'])
+def update_driver(id):
+
+    driver = request.get_json()
+    driversService = DriversService()
+    if not driver:
+        return jsonify({"error": "Driver not found"}), HTTPStatus.NOT_FOUND
+    resp = driversService.updateDriver(DriverSchema(id=id, **driver))
+    return jsonify(resp), HTTPStatus.OK
+
