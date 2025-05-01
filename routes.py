35648914b47339d3cd5d507628a30eb967aa4d03
Kevin Medina <kevinmedina2509Jr@gmail.com>
2025-04-30 19:09:40 -0600

    fix: routes.py fixed

--- routes.py
+++ routes.py
@@ -1,8 +1,8 @@
 from controllers import drivers
-from controllers import drivers, routes_controller, vehicle_controller, assignment_controller
+from controllers import drivers, routes_controller, vehicle_controller, assignment_controller, admin_controller
 
 def register_routes(app):
     app.register_blueprint(drivers.drivers_blueprint, url_prefix='/drivers')
     app.register_blueprint(vehicle_controller.vehicle_blueprint, url_prefix='/vehicle')
-    app.register_blueprint(vehicle_controller.vehicle_blueprint, url_prefix='/assignments')
+    app.register_blueprint(assignment_controller.assignment_blueprint, url_prefix='/assignments')
     app.register_blueprint(admin_controller.admin_blueprint, url_prefix='/admin')
