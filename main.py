e0abd4f696f18acdea9085050fc6c8383a381030
abrahamcruzc <abrahamcz_@hotmail.com>
2025-05-27 22:07:53 -0600

    add logging and monitoring infraestructure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify
 import os
+import logging
+import sys 
+from pythonjsonlogger import jsonlogger
 from dotenv import load_dotenv
 from routes import register_routes
 from utils.db import init_db
@@ -7,9 +10,21 @@ from flask_jwt_extended import JWTManager
 from datetime import timedelta
 from flask_cors import CORS
 
+log = logging.getLogger() 
+log.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+formatter = jsonlogger.JsonFormatter('%(asctime)s %(levelname)s %(module)s %(funcName)s %(lineno)d %(message)s')
+handler.setFormatter(formatter)
+log.addHandler(handler)
+
+werkzeug_logger = logging.getLogger('werkzeug')
+werkzeug_logger.addHandler(handler)
+werkzeug_logger.setLevel(logging.DEBUG) 
+
 init_db()
 load_dotenv()
 app = Flask(__name__)
+
 CORS(app,  
         origins=["http://localhost:5173", "http://127.0.0.1:5173"],
         allow_headers=["Content-Type", "Authorization"],
@@ -28,6 +43,12 @@ register_routes(app)
 @app.route('/')
 def root():
     """Simple root endpoint that returns API status"""
+    logger = logging.getLogger(__name__) 
+    logger.debug("This is a debug log from the root route.")
+    logger.info("This is an info log from the root route.")
+    logger.warning("This is a warning log from the root route.")
+    logger.error("This is an error log from the root route.")
+    logger.critical("This is a critical log from the root route.")
     return jsonify({
         "status": "API is running",
         "version": "1.0.0",
